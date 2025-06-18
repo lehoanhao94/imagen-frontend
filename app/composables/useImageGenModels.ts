32d77e1e5b63d581f7c3b59ca19ab1dc4e837633
Le Hoan Hao <hao.le@playnext-lab.co.jp>
2025-06-18 23:51:56 +0900

    update

--- app/composables/useImageGenModels.ts
+++ app/composables/useImageGenModels.ts
@@ -1,34 +1,34 @@
 export function useImageGenModels() {
   const models = [
     {
-      label: "Gemini 2.0 Flash",
-      value: "imagen-flash",
-      icon: "hugeicons:ai-chip",
-      options: ["style", "yourImage", "imageDimensions"],
+      label: 'Gemini 2.0 Flash',
+      value: 'imagen-flash',
+      icon: 'hugeicons:ai-chip',
+      options: ['style', 'yourImage', 'imageDimensions']
     },
     {
-      label: "Imagen 4",
-      value: "imagen-4",
-      icon: "hugeicons:ai-chip",
-      options: ["style", "imageDimensions"],
+      label: 'Imagen 4 Fast',
+      value: 'imagen-4-fast',
+      icon: 'hugeicons:ai-chip',
+      options: ['style', 'imageDimensions']
     },
     {
-      label: "Imagen 4 Fast",
-      value: "imagen-4-fast",
-      icon: "hugeicons:ai-chip",
-      options: ["style", "imageDimensions"],
+      label: 'Imagen 4',
+      value: 'imagen-4',
+      icon: 'hugeicons:ai-chip',
+      options: ['style', 'imageDimensions']
     },
     {
-      label: "Imagen 4 Ultra",
-      value: "imagen-4-ultra",
-      icon: "hugeicons:ai-chip",
-      options: ["style", "imageDimensions"],
-    },
-  ];
-  const model = useCookie<any>("image-gen-model", { default: () => models[0] });
+      label: 'Imagen 4 Ultra',
+      value: 'imagen-4-ultra',
+      icon: 'hugeicons:ai-chip',
+      options: ['style', 'imageDimensions']
+    }
+  ]
+  const model = useCookie<any>('image-gen-model', { default: () => models[0] })
 
   return {
     models,
-    model,
-  };
+    model
+  }
 }
