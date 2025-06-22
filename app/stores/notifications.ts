fdd68c185610c2d1872b51b968ee9e5f7c5e8d47
Hoodie.Le <lehoanhao.94@gmail.com>
2025-06-23 00:53:33 +0900

    Revert "Implement infinite scroll for NotificationSlideover with 20 item default limit"

--- app/stores/notifications.ts
+++ app/stores/notifications.ts
@@ -38,8 +38,8 @@ export const useNotificationsStore = defineStore('notificationsStore', {
     error: null,
     isFetching: false,
     totalCount: 0,
-    limit: 20,
-    range: { from: 0, to: 19 },
+    limit: 10,
+    range: { from: 0, to: 9 },
     realtimeChannel: null,
     openNotificationDrawer: false,
     notificationHistoryUuid: undefined
